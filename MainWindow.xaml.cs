ce45d98b18147fd61f17057f9c2c076cdd302e3c
mu <mu@MUYELINGYUE>
2024-04-15 19:27:43 +0800

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Write_DXLK
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Input_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (ofd.ShowDialog() == true)
+            {
+                string FileCintent = File.ReadAllText(ofd.FileName);
+                RichTextBox.Text = FileCintent;
+            }
+        }
+        
+    }
+}
